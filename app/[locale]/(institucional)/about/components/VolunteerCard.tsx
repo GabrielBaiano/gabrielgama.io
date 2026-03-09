c6d44f6fa8e486165f14579e9cfc114be7fd2a27
gabrielbaiano <gabrielngama@gmail.com>
2026-03-09 06:47:38 -0300

    style: reduce VolunteerCard text size to fit box

--- app/[locale]/(institucional)/about/components/VolunteerCard.tsx
+++ app/[locale]/(institucional)/about/components/VolunteerCard.tsx
@@ -7,9 +7,9 @@ export function VolunteerCard() {
         <motion.div
             className="bg-[#FB385E] text-white rounded-[24px] flex flex-col justify-center shadow-sm w-full h-full relative overflow-hidden group aspect-square p-4 md:p-6"
         >
-            <h3 className="text-[54px] sm:text-[64px] md:text-[60px] lg:text-[76px] font-extrabold leading-[0.95] tracking-[-0.04em] whitespace-nowrap -ml-1">
-                Volunte<br />
-                Teach
+            <h3 className="text-[38px] sm:text-[45px] md:text-[50px] lg:text-[60px] font-extrabold leading-[0.95] tracking-[-0.04em] break-words -ml-1 pr-2">
+                Volunteer<br />
+                Teacher
             </h3>
         </motion.div>
     );
